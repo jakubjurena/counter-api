0f9cc7acd44fccf1dfc5047409cecf83c1eea6c9
Jakub Juřena <jakub.jurena@gmail.com>
2018-02-14 14:34:14 +0100

    refactor PORT -> SERVER_PORT

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    PORT: process.env.PORT,
+    SERVER_PORT: process.env.PORT,
     DB_PORT: process.env.DB_PORT,
     DB_HOST: process.env.DB_HOST,
 });
--- config/test.js
+++ config/test.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    PORT: 3000,
+    SERVER_PORT: 3000,
     DB_PORT: 6379,
     DB_HOST: "127.0.0.1",
 });
