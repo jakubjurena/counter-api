b2ff03ab973dc91ef416eeafd55fa90786b9caa8
Jakub Juřena <jakub.jurena@gmail.com>
2018-02-15 00:18:39 +0100

    increment db and append file asynchronously

--- routes/counterRoutes.js
+++ routes/counterRoutes.js
@@ -1,17 +1,74 @@
 
-module.exports = (app, redisClient) => {
+const {COUNT_KEY} = require("../enums/dbKeys");
+const async = require("async");
+const fs = require("fs");
 
+module.exports = (app, redisClient) => {
+    
     app.get("/count", (req, res) => {
-        //TODO save JSON body to file (using body-parser)
-        //TODO increment value in Redis
-        const number = 0;
-        res.json({count : number});
+        //TODO read value from Redis
+        redisClient.get(COUNT_KEY, (err, value) => {
+            let count;
+            if (value) {
+                count = parseInt(value);
+            } else {
+                count = 0;
+            }
+            res.status(200).json({count});
+        })
     })
 
     app.post("/track", (req, res) => {
-        //TODO read value from Redis
-        const error = new Error("Server error");
-        res.status(500).json({error});
+
+        if (req.get("Content-Type") !== "application/json") {
+            return res.status(400).json({
+                errorMessage: "Invalid content type"
+            })
+        }
+
+
+        async.series({
+            increaseCount: function(callback) {
+                if (req.body.count) {
+                    const parsedCount = parseInt(req.body.count);
+
+                    if ( !parsedCount )
+                        return callback("parseError","Count have to be integer");
+                    if ( parsedCount < 0) 
+                        return callback("rangeError","Count can not be negative");
+
+                    redisClient.incrby(COUNT_KEY, parsedCount, (err, increasedValue) => {
+                        return callback(null, "Count successfully increased!");
+                    }) 
+                    
+                } else {
+                    callback(null, "Nothing increased");
+                }
+            },
+            fileAppend: function(callback) {
+                const data = JSON.stringify(req.body);
+                
+                fs.appendFile("actions.json", data, (err) => {
+                    if (err) return callback("fileError", "File can not be saved");
+                    
+                    callback(null, "File has been saved");
+                })
+            } 
+        }, (err, results) => {
+            console.log(JSON.stringify(results));
+
+            if (err) {
+                res.status(400);
+            } else {
+                res.status(200);
+            }
+
+            res.json({
+                countMessage: results.increaseCount,
+                fileMessage: results.fileAppend
+            })
+        })
+
     })
 
 };
